f9c1b5bb5264518843413d0446cab4454af15483
Andrew House <andrewhouse621@gmail.com>
2014-04-16 17:24:13 -0400

    still trying to fix aws uploading

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,9 +2,8 @@ class Picture < ActiveRecord::Base
   has_attached_file :image, 
   									:styles => { :medium => "300x300>", :thumb => "100x100>", :large => "600>" }, 
   									:storage => :s3,
-									  :s3_credentials => "#{RAILS.root}/config/s3.yml",
-									  :path => ":attachment/:id/:style.:extension",
-									  :bucket => "andrewhouse"
+									  :s3_credentials => "#{Rails.root}/config/s3.yml",
+									  :path => ":attachment/:id/:style.:extension"
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :username, presence: true
