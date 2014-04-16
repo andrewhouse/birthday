113c9d6b17d7cf168a5ecac33190dc9bed359cf0
Andrew House <andrewhouse621@gmail.com>
2014-04-15 23:10:56 -0400

    configuring aws

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,11 @@
 class Picture < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :large => "600>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, 
+  									:styles => { :medium => "300x300>", :thumb => "100x100>", :large => "600>" }, 
+  									:storage => :s3,
+									  :s3_credentials => "#{Rails.root}/config/s3.yml",
+									  :path => ":attachment/:id/:style.:extension",
+									  :bucket => "andrewhouse"
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :username, presence: true
   validates :description, presence: true
