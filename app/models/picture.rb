dc1dbcd5e9c3f0f204d35bdc69babf07ac21cfae
Andrew House <andrewhouse621@gmail.com>
2014-04-17 13:58:12 -0400

    upload issues

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,10 @@
 class Picture < ActiveRecord::Base
   has_attached_file :image, 
   									:styles => { :medium => "300x300>", :thumb => "100x100>", :large => "600>" }, 
-									  :path => ":attachment/:id/:style.:extension"
+									  :path => ":attachment/:id/:style.:extension",
+									  :s3_credentials => "#{Rails.root}/config/s3.yml",
+									  :storage => :s3
+
 
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
