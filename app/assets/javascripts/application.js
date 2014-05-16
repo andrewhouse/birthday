1127a196817a8699d88a5aa6795c54bd792c1294
Andrew House <andrewhouse621@gmail.com>
2014-05-16 12:29:45 -0400

    add carousel feature

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,7 +35,36 @@ function hoverLeaveTitle () {
 	$(this).css('color', '#08c')
 }
 
+function carouselNormalization() {
+var items = $('#carousel-example-generic .item'), //grab all slides
+    heights = [], //create empty array to store height values
+    tallest; //create variable to make note of the tallest slide
+
+	if (items.length) {
+	    function normalizeHeights() {
+	        items.each(function() { //add heights to array
+	            heights.push($(this).height()); 
+	        });
+	        tallest = Math.max.apply(null, heights); //cache largest value
+	        items.each(function() {
+	            $(this).css('min-height',tallest + 'px');
+	        });
+	    };
+	    normalizeHeights();
+
+	    $(window).on('resize orientationchange', function () {
+	        tallest = 0, heights.length = 0; //reset vars
+	        items.each(function() {
+	            $(this).css('min-height','0'); //reset min-height
+	        }); 
+	        normalizeHeights(); //run it again 
+	    });
+	}
+}
+
 $(document).ready(function(){
 	$('.containing-border').on('click','button', showButton);
 	$('.container').on('mouseenter','a', hoverTitle).on('mouseleave','a', hoverLeaveTitle);
+	$('.carousel').carousel({interval:4000, wrap: true, pause: ""});
+	carouselNormalization();
 });
